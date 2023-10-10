83693af765884a23490bddbb2fcc0f0a077e4357
MAYURI DODDANNAVAR <mdd922100@tatatechnologies.com>
2023-10-10 17:56:02 +0530

    Add project files.

---
+++ MVCCRUDProject/Migrations/202007191214057_UpdateEmployeeData.cs
@@ -0,0 +1,18 @@
+﻿namespace MVCCRUDProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateEmployeeData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into Employees(EmployeeName,EmployeeDesignation,EmployeeAddress,EmployeePassport,EmployeePhone,EmployeeGender,City,Project,CompanyName,PinCode,DepartmentId)" +
+                "Values('Mayuri','Solution Developer','Pune','Mayu1234',9876543210,'Female','Pune','Developer','Tata Technologies',411057,1)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MVCCRUDProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("MVCCRUDProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCCRUDProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b185fa7-5218-4b68-a3cd-85e2d24e04c0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
